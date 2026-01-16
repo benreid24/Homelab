b8a4408d8f0461ead38c00a5a121a1a73773dc83
Ben Reid <reidben24@gmail.com>
2026-01-15 22:04:32 -0500

    Run some containers as service user. Move seafile to role

--- ansible/playbooks/roles/service_redbot/files/redbot/entrypoint.sh
+++ ansible/playbooks/roles/service_redbot/files/redbot/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
+# Ensure data directory has correct ownership
+# Container starts as root, so this will work
 chown -R ${RED_USER}:${RED_USER} ${RED_HOME}/data
 
 # Install pip packages and start Red as specified user
